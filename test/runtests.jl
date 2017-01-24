643d9428105ae53eec877201331a0cecf0cbf4cc
Mosè Giordano <mose@gnu.org>
2017-01-24 18:16:24 +0100

    Loosen tolerance in tests for 64-bit integer functions

--- test/runtests.jl
+++ test/runtests.jl
@@ -57,8 +57,8 @@ for (alg, abstol) in ((vegas, 1e-4), (suave, 1e-3),
 end
 
 # Test 64-bit integer functions.
-for (alg, abstol) in ((llvegas, 2e-5), (llsuave, 6e-6),
-                      (lldivonne, 7e-7), (llcuhre, 1e-8))
+for (alg, abstol) in ((llvegas, 1e-4), (llsuave, 1e-3),
+                      (lldivonne, 1e-4), (llcuhre, 1e-8))
     # Make sure that using maxevals > typemax(Int32) doesn't result into InexactError.
     result = alg((x,f) -> f[1] = f1(x[1], x[2], x[3]), 3, maxevals = 3e9)
     @test_approx_eq_eps result[1][1] answer[1] abstol
